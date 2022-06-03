cbcd6219abe2232a92802881edd68f3d4a47744a
Disni Jayawickrama <86881451+Disnidj@users.noreply.github.com>
2022-06-04 04:04:22 +0530

    commit8

--- src/components/Groupids.js
+++ src/components/Groupids.js
@@ -9,88 +9,88 @@ export default function Groupids()  {
 
  
 
-    return (
-        <div>
-         <div>
+  return (
+    <div>
+     <div>
 
-         <br/>
-            <center><h1>Student Groups - Panel Formations</h1></center> 
-          <br/>
+     <br/>
+        <center><h1 style={{background:'#000080', color:'white',fontSize:'50px'}}>Student Groups - Panel Formations</h1></center> 
+      <br/>
 
 
-      
-        <table className = "table">
-           <thead>
-             <tr>
-               <th scope = "col">Group ID</th>
-               <th scope = "col">Students</th>
-               <th scope = "col">Topic Panel</th>
-               <th scope = "col">Presentation Panel</th>
-               <th scope = "col">Thesis Panel</th>
-           
-             </tr>
-           </thead>
-            <tbody>
-    
-            
-      
-                <tr><th>Group_1</th>
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td> </tr>
-                <tr><th>Group_2</th> 
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td> </tr>
-                <tr><th>Group_3</th>
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
-                <tr><th>Group_4</th>
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
-                <tr><th>Group_5</th> 
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td> </tr>
-                <tr><th>Group_6</th>
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
-                <tr><th>Group_7</th>
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
-                <tr><th>Group_8</th>
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
-                <tr><th>Group_9</th>
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
-                <tr><th>Group_10</th>
-                <td><a  href={`/students`}>students</a></td>
-                <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
-                <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
-                  </tbody>
-                 </table>
+  
+    <table className = "table" style={{backgroundColor:'#d0efff'}}>
+       <thead>
+         <tr>
+           <th scope = "col">Group ID</th>
+           <th scope = "col">Students</th>
+           <th scope = "col">Topic Panel</th>
+           <th scope = "col">Presentation Panel</th>
+           <th scope = "col">Thesis Panel</th>
+       
+         </tr>
+       </thead>
+        <tbody>
 
-              
-        </div>
-        </div>
+        
+  
+            <tr><th>Group_1</th>
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td> </tr>
+            <tr><th>Group_2</th> 
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td> </tr>
+            <tr><th>Group_3</th>
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
+            <tr><th>Group_4</th>
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
+            <tr><th>Group_5</th> 
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td> </tr>
+            <tr><th>Group_6</th>
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
+            <tr><th>Group_7</th>
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
+            <tr><th>Group_8</th>
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
+            <tr><th>Group_9</th>
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
+            <tr><th>Group_10</th>
+            <td><a  href={`/view`}>students</a></td>
+            <td><a  href={`/AddTopisPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddPresentationPanels`}>Assging  Panel</a></td>
+            <td><a  href={`/AddThesisPanels`}>Assging  Panel</a></td>  </tr>
+              </tbody>
+             </table>
 
-    );
+          
+    </div>
+    </div>
+
+);
   }
 
